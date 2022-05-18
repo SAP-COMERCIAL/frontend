535e84b73685d4de61479aab58a3f093272c4a9e
Juan Jose González Ugalde <josej.gonzalez.ext@oxxogas.com>
2022-05-18 16:00:39 -0500

    changes

---
+++ dist/src_app_pages_invoice_invoice_module_ts-es5.js
@@ -0,0 +1,137 @@
+(function () {
+  function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+  (self["webpackChunkmaterial"] = self["webpackChunkmaterial"] || []).push([["src_app_pages_invoice_invoice_module_ts"], {
+    /***/
+    21259:
+    /*!*************************************************!*\
+      !*** ./src/app/pages/invoice/invoice.module.ts ***!
+      \*************************************************/
+
+    /***/
+    function _(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+      "use strict";
+
+      __webpack_require__.r(__webpack_exports__);
+      /* harmony export */
+
+
+      __webpack_require__.d(__webpack_exports__, {
+        /* harmony export */
+        "InvoiceModule": function InvoiceModule() {
+          return (
+            /* binding */
+            _InvoiceModule
+          );
+        }
+        /* harmony export */
+
+      });
+      /* harmony import */
+
+
+      var _angular_router__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(
+      /*! @angular/router */
+      39895);
+      /* harmony import */
+
+
+      var _angular_common__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(
+      /*! @angular/common */
+      38583);
+      /* harmony import */
+
+
+      var _angular_flex_layout__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(
+      /*! @angular/flex-layout */
+      25830);
+      /* harmony import */
+
+
+      var ng_chartist__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(
+      /*! ng-chartist */
+      14735);
+      /* harmony import */
+
+
+      var _invoice_routing__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(
+      /*! ./invoice.routing */
+      35191);
+      /* harmony import */
+
+
+      var _demo_material_module__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(
+      /*! ../../demo-material-module */
+      55408);
+      /* harmony import */
+
+
+      var _angular_core__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(
+      /*! @angular/core */
+      37716);
+
+      var _InvoiceModule = function _InvoiceModule() {
+        _classCallCheck(this, _InvoiceModule);
+      };
+
+      _InvoiceModule.ɵfac = function InvoiceModule_Factory(t) {
+        return new (t || _InvoiceModule)();
+      };
+
+      _InvoiceModule.ɵmod = /*@__PURE__*/_angular_core__WEBPACK_IMPORTED_MODULE_2__["ɵɵdefineNgModule"]({
+        type: _InvoiceModule
+      });
+      _InvoiceModule.ɵinj = /*@__PURE__*/_angular_core__WEBPACK_IMPORTED_MODULE_2__["ɵɵdefineInjector"]({
+        imports: [[_angular_common__WEBPACK_IMPORTED_MODULE_3__.CommonModule, _demo_material_module__WEBPACK_IMPORTED_MODULE_1__.DemoMaterialModule, _angular_flex_layout__WEBPACK_IMPORTED_MODULE_4__.FlexLayoutModule, ng_chartist__WEBPACK_IMPORTED_MODULE_5__.ChartistModule, _angular_router__WEBPACK_IMPORTED_MODULE_6__.RouterModule.forChild(_invoice_routing__WEBPACK_IMPORTED_MODULE_0__.InvoiceRoutes)]]
+      });
+
+      (function () {
+        (typeof ngJitMode === "undefined" || ngJitMode) && _angular_core__WEBPACK_IMPORTED_MODULE_2__["ɵɵsetNgModuleScope"](_InvoiceModule, {
+          imports: [_angular_common__WEBPACK_IMPORTED_MODULE_3__.CommonModule, _demo_material_module__WEBPACK_IMPORTED_MODULE_1__.DemoMaterialModule, _angular_flex_layout__WEBPACK_IMPORTED_MODULE_4__.FlexLayoutModule, ng_chartist__WEBPACK_IMPORTED_MODULE_5__.ChartistModule, _angular_router__WEBPACK_IMPORTED_MODULE_6__.RouterModule]
+        });
+      })();
+      /***/
+
+    },
+
+    /***/
+    35191:
+    /*!**************************************************!*\
+      !*** ./src/app/pages/invoice/invoice.routing.ts ***!
+      \**************************************************/
+
+    /***/
+    function _(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+      "use strict";
+
+      __webpack_require__.r(__webpack_exports__);
+      /* harmony export */
+
+
+      __webpack_require__.d(__webpack_exports__, {
+        /* harmony export */
+        "InvoiceRoutes": function InvoiceRoutes() {
+          return (
+            /* binding */
+            _InvoiceRoutes
+          );
+        }
+        /* harmony export */
+
+      });
+      /* harmony import */
+
+
+      var _invoice_component__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(
+      /*! ./invoice.component */
+      91046);
+
+      var _InvoiceRoutes = [{
+        path: '',
+        component: _invoice_component__WEBPACK_IMPORTED_MODULE_0__.InvoiceComponent
+      }];
+      /***/
+    }
+  }]);
+})();
+//# sourceMappingURL=src_app_pages_invoice_invoice_module_ts-es5.js.map
